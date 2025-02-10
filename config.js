27114380754a7443315e8c95d29c00bdae06610c
NethminaPansil <nethminapansilu5@gmail.com>
2025-02-10 21:35:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "qjRz1BoS#C5SJPfDeF2wJ3m4KrVlWFPoDoQECaJAhp2IYDT0AjEY",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:vXILNtmpUbvOJCqUvqjlGAvEbyLLfZjk@mongodb.railway.internal:27017",
+  OWNER_NUM: process.env.OWNER_NUM || "94763513529",
 };
