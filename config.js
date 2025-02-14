e481038667e1fb14225dbc5371d27a55caa18dfa
NethminaPansil <nethminapansilu5@gmail.com>
2025-02-14 10:29:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "qjRz1BoS#C5SJPfDeF2wJ3m4KrVlWFPoDoQECaJAhp2IYDT0AjEY",
+  SESSION_ID: process.env.SESSION_ID || "m3523J4R#hFweCit7r0ggx_G2LTCvPYiRYmZSxEGL8UgGeHSIC2U",
   MONGODB: process.env.MONGODB || "mongodb://mongo:vXILNtmpUbvOJCqUvqjlGAvEbyLLfZjk@viaduct.proxy.rlwy.net:18403",
   OWNER_NUM: process.env.OWNER_NUM || "94763513529",
 };
