73637c363f5f0f8bb2fb457bda43a837625b055d
NethminaPansil <nethminapansilu5@gmail.com>
2025-02-16 22:19:19 +0530

    Update fbdown.js

--- plugins/fbdown.js
+++ plugins/fbdown.js
@@ -5,7 +5,7 @@ cmd(
   {
     pattern: "fb",
     alias: ["facebook"],
-    react: "💀",
+    react: "🫥",
     desc: "Download Facebook Video",
     category: "download",
     filename: __filename,
@@ -65,13 +65,13 @@ cmd(
 👻 *Title*: ${title || "Unknown"}
 👻 *Quality*: ${hd ? "HD Available" : "SD Only"}
 
-𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋
+𝐌𝐚𝐝𝐞 𝐛𝐲 𝙋𝘼𝙉𝙎𝙄𝙇𝙐
         `;
       await robin.sendMessage(
         from,
         {
           image: {
-            url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20fb-1.jpg",
+            url: "https://raw.githubusercontent.com/NethminaPansil/Whtsapp-bot/refs/heads/main/images%20(10).jpeg",
           },
           caption: desc,
         },
