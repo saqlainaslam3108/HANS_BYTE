5d930df774ad6836a60b40aebad8a6252c3f76a8
NethminaPansil <nethminapansilu5@gmail.com>
2025-02-16 20:07:43 +0530

    Update YTvideo.js

--- plugins/YTvideo.js
+++ plugins/YTvideo.js
@@ -1,6 +1,6 @@
 const { cmd, commands } = require("../command");
 const yts = require("yt-search");
-const { ytmp4 } = require("");
+const { ytmp4 } = require("@vreden/youtube_scraper");
 
 cmd(
   {
