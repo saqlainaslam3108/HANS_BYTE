738e73b393ec234416ac1133e64b1d122683f523
NethminaPansil <nethminapansilu5@gmail.com>
2025-02-17 15:28:13 +0530

    Update img2sticker.js

--- plugins/img2sticker.js
+++ plugins/img2sticker.js
@@ -6,6 +6,7 @@ cmd(
   {
     pattern: "sticker",
     alias: ["s", "stick"],
+    react: "🥱",
     desc: "Convert an image to a sticker",
     category: "utility",
     filename: __filename,
@@ -53,8 +54,8 @@ cmd(
 
       // Create the sticker from the media
       const sticker = new Sticker(media, {
-        pack: "𝐑_𝐎_𝐁_𝐈_𝐍", // Sticker pack name
-        author: "𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋", // Sticker author name
+        pack: "卩卂几丂丨ㄥㄩ", // Sticker pack name
+        author: "几乇ㄒ卄爪丨几卂", // Sticker author name
         type: StickerTypes.FULL, // Sticker type (FULL or CROPPED)
         quality: 50, // Quality of the output sticker (0–100)
       });
