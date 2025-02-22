c93e0f951da374768c8dadde8e41533226ba87bc
Pansilu Nethmina <nethminapansilu5@gmail.com>
2025-02-22 13:23:48 +0530

    Update vv.js

--- plugins/vv.js
+++ plugins/vv.js
@@ -2,15 +2,15 @@ const { cmd, forwardOrBroadCast, parsedJid } = require('../command');
 
 cmd(
   {
-    pattern: 'vv ?(.*)',
+    pattern: '.vv ?(.*)',
     fromMe: true,
-    desc: 'antiViewOnce - View Once image, video, or audio re-send without view-once restriction',
+    desc: 'antiViewOnce - View Once media re-send without view-once restriction',
     type: 'whatsapp',
     category: 'utility',
     filename: __filename,
   },
   async (message, match) => {
-    // පලමුව reply message එකක් තිබෙනවාද කියලා පරීක්ෂා කරන්න
+    // Reply message එකේ media තිබේද කියලා පරීක්ෂා කරන්න
     if (
       !message.reply_message ||
       (!message.reply_message.image &&
@@ -18,11 +18,11 @@ cmd(
         !message.reply_message.audio)
     ) {
       return await message.send(
-        '*උදාහරණ:*\n- viewOnce image, video, හෝ audio එකකට reply කරන්න\n- vv jid (විකල්ප ලෙස jid එකද ලබා දිය හැක)'
+        '*උදාහරණ:*\n- viewOnce image, video, හෝ audio එකකට reply කරන්න\n- .vv jid (විකල්ප jid එකක්)'
       );
     }
 
-    // jid එක parsedJid function එකෙන් extract කිරීම
+    // parsedJid function එකෙන් jid extract කිරීම
     const [jid] = parsedJid(match);
     
     // forwardOrBroadCast function එකෙන් viewOnce restriction අහෝසි කිරීම
