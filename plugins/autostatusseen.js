5cb6e12756b542869ad6383af16eb3f20c7ba983
Pansilu Nethmina <nethminapansilu5@gmail.com>
2025-02-22 16:23:24 +0530

    Update autostatusseen.js

--- plugins/autostatusseen.js
+++ plugins/autostatusseen.js
@@ -1,8 +1,18 @@
-module.exports = (sock) => {
-  sock.ev.on('messages.upsert', async ({ messages, type }) => {
-    if (type === 'notify') {
-      const message = messages[0]
-      await sock.sendReadReceipt(message.key.remoteJid, message.key.id)
-    }
-  })
+module.exports = {
+  name: 'autoStatusSeen',
+  type: 'auto',
+  description: 'Automatically marks statuses as seen',
+  async onLoad(sock) {
+    sock.ev.on('messages.upsert', async ({ messages, type }) => {
+      if (type === 'notify') {
+        const message = messages[0]
+        try {
+          await sock.sendReadReceipt(message.key.remoteJid, message.key.id)
+          console.log(`[AUTO SEEN] Marked status as seen for: ${message.key.remoteJid}`)
+        } catch (err) {
+          console.error('[AUTO SEEN ERROR]', err)
+        }
+      }
+    })
+  }
 }
