f5a77f73f1dfdd436c0bc622cdb5c853ddce5f0e
Pansilu Nethmina <nethminapansilu5@gmail.com>
2025-02-22 13:15:32 +0530

    Update vv.js

--- plugins/vv.js
+++ plugins/vv.js
@@ -1,17 +1,23 @@
-const { makeWASocket, proto } = require('@whiskeysockets/baileys'); const { bot, parsedJid } = require('../lib');
+const { bot, forwardOrBroadCast, parsedJid } = require('../command');
 
-bot( { pattern: 'vv ?(.*)', fromMe: true, desc: 'Remove view once restriction from media', type: 'whatsapp', }, async (message, match) => { if (!message.reply_message || !(message.reply_message.image || message.reply_message.video || message.reply_message.audio)) { return await message.send('Reply to a view once image, video, or audio with the command: vv [jid (optional)]'); }
-
-const jid = parsedJid(match)[0] || message.jid;
-
-const msg = proto.WebMessageInfo.fromObject(message.reply_message.data);
-if (msg.message?.viewOnceMessage?.message) {
-  msg.message = msg.message.viewOnceMessage.message;
-  msg.viewOnce = false;
-  await message.client.sendMessage(jid, msg.message, { quoted: message });
-} else {
-  await message.send('*This message is not a view once media*');
-}
-
-} );
-
+bot(
+  {
+    pattern: 'vv ?(.*)',
+    fromMe: true,
+    desc: 'antiViewOnce',
+    type: 'whatsapp',
+  },
+  async (message, match) => {
+    if (
+      !message.reply_message.image &&
+      !message.reply_message.video &&
+      !message.reply_message.audio
+    ) {
+      return await message.send(
+        '*Example\n- reply to a viewOnce image, video or audio*\n- vv jid (optional)'
+      );
+    }
+    const [jid] = parsedJid(match);
+    await forwardOrBroadCast(jid || message.jid, message, { viewOnce: false });
+  }
+);
