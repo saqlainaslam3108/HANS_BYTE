d504be42fea201c1c4ab5b847a97b5fcb44be909
Pansilu Nethmina <nethminapansilu5@gmail.com>
2025-02-22 13:19:29 +0530

    Update vv.js

--- plugins/vv.js
+++ plugins/vv.js
@@ -1,23 +1,31 @@
-const { bot, forwardOrBroadCast, parsedJid } = require('../command');
+const { cmd, forwardOrBroadCast, parsedJid } = require('../command');
 
-bot(
+cmd(
   {
     pattern: 'vv ?(.*)',
     fromMe: true,
-    desc: 'antiViewOnce',
+    desc: 'antiViewOnce - View Once image, video, or audio re-send without view-once restriction',
     type: 'whatsapp',
+    category: 'utility',
+    filename: __filename,
   },
   async (message, match) => {
+    // පලමුව reply message එකක් තිබෙනවාද කියලා පරීක්ෂා කරන්න
     if (
-      !message.reply_message.image &&
-      !message.reply_message.video &&
-      !message.reply_message.audio
+      !message.reply_message ||
+      (!message.reply_message.image &&
+        !message.reply_message.video &&
+        !message.reply_message.audio)
     ) {
       return await message.send(
-        '*Example\n- reply to a viewOnce image, video or audio*\n- vv jid (optional)'
+        '*උදාහරණ:*\n- viewOnce image, video, හෝ audio එකකට reply කරන්න\n- vv jid (විකල්ප ලෙස jid එකද ලබා දිය හැක)'
       );
     }
+
+    // jid එක parsedJid function එකෙන් extract කිරීම
     const [jid] = parsedJid(match);
+    
+    // forwardOrBroadCast function එකෙන් viewOnce restriction අහෝසි කිරීම
     await forwardOrBroadCast(jid || message.jid, message, { viewOnce: false });
   }
 );
