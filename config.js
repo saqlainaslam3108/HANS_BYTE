bb273ccd32bf8be8a2ab640dc4af6d31e891c78a
Pansilu Nethmina <nethminapansilu5@gmail.com>
2025-02-22 14:51:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   PREFIX:process.env.PREFIX || ".",
   ALIVE_IMG:process.env.ALIVE_IMG||"https://raw.githubusercontent.com/NethminaPansil/Whtsapp-bot/refs/heads/main/images%20(11).jpeg",
   ALIVE_MSG:process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n Join my Support group using this Link 📥 \n\n https://chat.whatsapp.com/EXPFUXpHxFR5ur03emaNx0 \n\n 💝 𝐌𝐚𝐝𝐞 𝐛𝐲 PANSILU 💝 \n\n 🔒 ᐯㄖ尺ㄒ乇乂 爪ᗪ ㄒ乇卂爪 🔒 ",
-  AUTO_READ_STATUS:process.env.AUTO_READ_STATUS|| "true",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   MODE:process.env.MODE || "public",
   AUTO_VOICE:process.env.AUTO_VOICE|| "true",
   AUTO_STICKER:process.env.AUTO_STICKER|| "true",
