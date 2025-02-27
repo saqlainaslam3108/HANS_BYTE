f352a0deee4a4acc5bc5ea6fa335d06727435407
Pansilu Nethmina <nethminapansilu5@gmail.com>
2025-02-27 15:43:16 +0530

    Create ar.js

---
+++ plugins/ar.js
@@ -0,0 +1,20 @@
+//------- *STATUS AUTO REACT* ----------
+
+conn.ev.on('messages.upsert', async(mek) => {
+mek = mek.messages[0]
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+      await conn.readMessages([mek.key])
+    }
+  if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+    const emojis = ['🧩', '🍉', '💜', '🌸', '🪴', '💊', '💫', '🍂', '🌟', '🎋', '😶‍🌫️', '🫀', '🧿', '👀', '🤖', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💚'];
+    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+    await conn.sendMessage(mek.key.remoteJid, {
+      react: {
+        text: randomEmoji,
+        key: mek.key,
+      } 
+    }, { statusJidList: [mek.key.participant] });
+  }
+
+
+//----```© Pink Venom ofc```-----------🗿🤌
