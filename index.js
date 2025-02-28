871671469764a2e0d3f2d87888fc6720eeb13110
Pansilu Nethmina <nethminapansilu5@gmail.com>
2025-02-28 07:32:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -243,12 +243,6 @@ if (
     };
 
 
-//auto react 
-
-conn.ev.on('messages.upsert', async (mek) => {
-    const statusAutoReact = require('./plugins/statusAutoReact');
-    await statusAutoReact(conn, mek, config);
-});
   
 //owner react
 
