38d144bb4f3e22390c7c2731a6ae277708d7c38e
HaroldMth <167818233+HaroldMth@users.noreply.github.com>
2025-05-24 03:17:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ if (fs.existsSync("config.env")) {
 // Application Configuration Object
 module.exports = {
   // Session and Owner Information
-  SESSION_ID: process.env.SESSION_ID || "HANS-BYTE~9uNUQYRL#iTR9ayZtYxwUPiyyqDSeV9GcpwasFaN5Y63vlHaJymE", // Add your session ID here
+  SESSION_ID: process.env.SESSION_ID || "", // Add your session ID here
   SUDO: process.env.SUDO || "237696900612", // Add your admin/sudo number here
   OWNER_NUM: process.env.OWNER_NUM || "237680260772", // Add the owner's number here
   OWNER_NAME: process.env.OWNER_NAME || "HANS TECH", // Add the owner's name here
